fe856a4af51cc9318c8a4c68e088026ba366960f
rifai <alliiyassir@gmail.com>
2025-08-17 09:10:23 +0100

    Adding sh.pages to Github

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/bolt-testt/", // ✅ repo name goes here
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
